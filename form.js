37b40ae14c5774676d4c43a83c436c700c536153
Dominika Wojewska <112077394+WitchDevelops@users.noreply.github.com>
2023-03-12 22:03:52 +0100

    add form endpoint and message

---
+++ form.js
@@ -0,0 +1,6 @@
+function sendEmail() {
+    let form = document.getElementById("contact-mail"); //get the form element and store it in a "form" variable
+    let submitted = document.createElement("p"); //create a new paragraph and store it in a "submitted" variable
+    submitted.innerText = "Your message was send! Thank you." // create some text
+    document.form.appendChild(submitted); //add it to the end of the form
+}
